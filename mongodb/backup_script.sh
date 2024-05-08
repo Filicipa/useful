93c4a700cfa5a1080ae004be5c5dfaded0ad7527
Alex <111982862+Filicipa@users.noreply.github.com>
2024-05-08 21:54:19 +0300

    Update

---
+++ mongodb/backup_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Backup MongoDB databases
+# Remove backups that are older than 5 days
+
+# Set backup directory and file name
+BACKUP_DIR="/data/backup_omomo_mongo"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_NAME="mongodb_backup_$DATE"
+
+# Run mongodump command to create backup
+mongodump --authenticationDatabase=admin --username=name \
+--password=1234 --db=base --out $BACKUP_DIR/$BACKUP_NAME --gzip
+
+# Set the number of backups to keep
+num_backups_to_keep=5
+
+# Remove backups more than 5
+cd $BACKUP_DIR
+ls -1t | grep "^mongodb_backup_" | tail -n +$((num_backups_to_keep+1)) \
+  | xargs -d '\n' rm -R -f
