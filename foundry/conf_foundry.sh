d85bc9521d42fb94973e1ae552dc0178caa2f08e
Alex <111982862+Filicipa@users.noreply.github.com>
2024-06-30 11:24:44 +0300

    Update

---
+++ foundry/conf_foundry.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ubuntu/.foundry/bin/anvil --host 0.0.0.0 --state /home/ubuntu/state/statev6.json --balance 1000000000 --disable-block-gas-limit
--- set_time.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-TZ="Europe/Zaporozhye" date "+%Y-%m-%d %H:%M:%S %Z"
