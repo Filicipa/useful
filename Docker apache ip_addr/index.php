aa1a43db978f7f7b0e34c3f9e711994bff2b3aa9
Alex <111982862+Filicipa@users.noreply.github.com>
2024-06-30 11:30:56 +0300

    upd

---
+++ Docker apache ip_addr/index.php
@@ -0,0 +1,6 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hellow from Kubernetes</h1><br>";
+echo "Server IP Address is: $ip_server", "<br>";
+echo "Made by <font color=blue>Filatov";
