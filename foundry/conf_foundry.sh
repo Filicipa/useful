e920554574a952a9590d72e6d36bb1d1d8883493
Filin <filicipa@gmail.com>
2025-01-03 11:28:06 +0200

    Update

--- foundry/conf_foundry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/home/ubuntu/.foundry/bin/anvil --host 0.0.0.0 --state /home/ubuntu/state/statev6.json --balance 1000000000 --disable-block-gas-limit
-
-/home/ubuntu/.foundry/bin/anvil --host 0.0.0.0 --fork-chain-id 137 --no-rate-limit -f https://polygon-mainnet.g.alchemy.com/v2/NlZ2xJx1w0vxWONvIWd74IWZXh8tBroW --fork-block-number 63432900 --block-time 3
