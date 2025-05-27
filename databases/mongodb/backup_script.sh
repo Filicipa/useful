a27bc934c7b0f6e09943dbb587a478dc517d1a80
Filin <filicipa@gmail.com>
2025-05-27 14:31:07 +0300

    Update Caddy

--- databases/mongodb/backup_script.sh
+++ databases/mongodb/backup_script.sh
@@ -9,12 +9,12 @@ BACKUP_NAME="mongodb_backup_$DATE"
 
 # Run mongodump command to create backup
 mongodump --authenticationDatabase=admin --username=name \
---password=1234 --db=base --out $BACKUP_DIR/$BACKUP_NAME --gzip
+--password=1234 --db=base --out $BACKUP_DIR/"$BACKUP_NAME" --gzip
 
 # Set the number of backups to keep
 num_backups_to_keep=5
 
 # Remove backups more than 5
-cd $BACKUP_DIR
+cd "$BACKUP_DIR" || exit
 ls -1t | grep "^mongodb_backup_" | tail -n +$((num_backups_to_keep+1)) \
   | xargs -d '\n' rm -R -f
